08f7c5b3f4fe00e85f8073e7d26a6e4d730f94f6
evanmarshall-dev <me@evanmarshall.dev>
2023-11-14 22:54:16 -0400

    📝 docs(binary-search): add notes on binary search and interview problem 📝 docs(binary-search): add interview problem description

---
+++ courses/modern-javascript/2. algorithms/1. binary-search/bs.js
@@ -0,0 +1,9 @@
+// One way to get an index of an item in a sorted array is to simply loop through the array until you find the item or target with the matching value.
+// Problem with this method is that it is not very efficient especially if the array is very large.
+// ? const arr = ['a', 'b', 'c', 'x', 'y', 'z'];
+
+// ?export default function search(arr, target) {
+//   ?for (let i = 0; i < arr.length; i += 1) {
+//     ?if (arr[i] === target) return i;
+//   ?}
+// ?}
--- courses/modern-javascript/2. algorithms/1. binary-search/notes.md
+++ courses/modern-javascript/2. algorithms/1. binary-search/notes.md
@@ -1,3 +1,7 @@
 # Binary Search
 
 ## Notes
+
+### Problems You Might Face on a Tech Interview
+
+Q. Create a function that takes a sorted array and a target value. Return the index of the target value in the array. If the target value is not in the array, return -1.
