fdb3b04544507c542222998f456a0684a66cf74d
evanmarshall-dev <me@evanmarshall.dev>
2023-11-20 08:09:08 -0400

    FIRESHIP.IO - JAVASCRIPT: Midway completed binary search lecture.

--- courses/modern-javascript/2. algorithms/1. binary-search/bs.js
+++ courses/modern-javascript/2. algorithms/1. binary-search/bs.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+
 // One way to get an index of an item in a sorted array is to simply loop through the array until you find the item or target with the matching value.
 // Problem with this method is that it is not very efficient especially if the array is very large.
 // ? const arr = ['a', 'b', 'c', 'x', 'y', 'z'];
@@ -7,3 +9,33 @@
 //     ?if (arr[i] === target) return i;
 //   ?}
 // ?}
+
+// Binary Search.
+// Default values set for start and end parameters so that we start at index 0 and end at the last index of the array.
+export default function search(arr, target, start = 0, end = arr.length - 1) {
+  console.log(start, end);
+
+  // Conditional for if start is greater than end then the value has not been found. It means it searched through the entire array and did not find the value.
+  if (start > end) {
+    console.log('value not found');
+    // If value not found return -1.
+    return -1;
+  }
+
+  // Most important part of this algorithm is to find the middle of the array.
+  // We do this by adding the start and end and dividing by 2. We also use Math.floor to round down to the nearest whole number.
+  const middle = Math.floor((start + end) / 2);
+
+  // Three things can now happen and we will setup conditionals for each.
+  // 1. The middle index can equal the target. If this is true then we have found the value and we can return the index.
+  if (arr[middle] === target) {
+    console.log(`${target} found at index ${middle}`);
+    return middle;
+  }
+  // 2. The middle index can be greater than the target.
+  // ? if (arr[middle] > target) {
+  // ? }
+  // 3. The middle index can be less than the target.
+  // ? if (arr[middle] < target) {
+  // ? }
+}
--- courses/modern-javascript/2. algorithms/1. binary-search/notes.md
+++ courses/modern-javascript/2. algorithms/1. binary-search/notes.md
@@ -5,3 +5,11 @@
 ### Problems You Might Face on a Tech Interview
 
 Q. Create a function that takes a sorted array and a target value. Return the index of the target value in the array. If the target value is not in the array, return -1.
+
+A. You could simply loop over the array until you get the target value, but this isn't efficient especially for large arrays.
+
+Thinking of algorithm complexity the above method would be **_O(N) Time Complexity_** (O of N). This means the worst amount of time it will take is equal to the elements in the array.
+
+A. Binary Search offers a more efficient approach.
+
+Analogous to looking for something with "J" in the dictionary. You would not start at the beginning and read through until you got there. You would open in the middle and if you over-shoot you would go back to J. And vise versa for if you under-shoot.
